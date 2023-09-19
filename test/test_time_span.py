be1ce45d4fdccc9e4938cc8c1fa1429252c6a7eb
Valentin Stauber <v.stauber@parityqc.com>
2023-09-19 17:35:16 +0200

    test TimeSpan.intersection

--- test/test_time_span.py
+++ test/test_time_span.py
@@ -7,24 +7,24 @@ from datetime import datetime, time, timedelta
     "span1, span2",
     [
         (
-            TimeSpan(start=datetime(2000, 1, 1, 10), end=datetime(2000, 1, 1, 11)),
-            TimeSpan(start=datetime(2000, 1, 1, 10), end=datetime(2000, 1, 1, 11)),
+                TimeSpan(start=datetime(2000, 1, 1, 10), end=datetime(2000, 1, 1, 11)),
+                TimeSpan(start=datetime(2000, 1, 1, 10), end=datetime(2000, 1, 1, 11)),
         ),
         (
-            TimeSpan(start=datetime(2000, 1, 1, 10), end=timedelta(hours=1)),
-            TimeSpan(start=datetime(2000, 1, 1, 10), end=timedelta(hours=1)),
+                TimeSpan(start=datetime(2000, 1, 1, 10), end=timedelta(hours=1)),
+                TimeSpan(start=datetime(2000, 1, 1, 10), end=timedelta(hours=1)),
         ),
         (
-            TimeSpan(start=datetime(2000, 1, 1, 10), end=timedelta(hours=1)),
-            TimeSpan(start=datetime(2000, 1, 1, 10), end=datetime(2000, 1, 1, 11)),
+                TimeSpan(start=datetime(2000, 1, 1, 10), end=timedelta(hours=1)),
+                TimeSpan(start=datetime(2000, 1, 1, 10), end=datetime(2000, 1, 1, 11)),
         ),
         (
-            TimeSpan(start=datetime(2000, 1, 1, 10), end=timedelta(hours=1)),
-            TimeSpan(start=datetime(2000, 1, 1, 10), end=timedelta(minutes=60)),
+                TimeSpan(start=datetime(2000, 1, 1, 10), end=timedelta(hours=1)),
+                TimeSpan(start=datetime(2000, 1, 1, 10), end=timedelta(minutes=60)),
         ),
         (
-            TimeSpan(start=time(10), end=timedelta(hours=1)),
-            TimeSpan(start=time(10), end=timedelta(minutes=60)),
+                TimeSpan(start=time(10), end=timedelta(hours=1)),
+                TimeSpan(start=time(10), end=timedelta(minutes=60)),
         ),
     ],
 )
@@ -32,54 +32,83 @@ def test_equality(span1, span2):
     assert span1 == span2
 
 
+@pytest.mark.parametrize('span1, span2, intersection', [
+    (
+            TimeSpan(start=datetime(1, 1, 1, 10), end=datetime(1, 1, 1, 11)),
+            TimeSpan(start=datetime(1, 1, 1, 10), end=datetime(1, 1, 1, 11)),
+            TimeSpan(start=datetime(1, 1, 1, 10), end=datetime(1, 1, 1, 11)),
+    ),
+    (
+            TimeSpan(start=datetime(1, 1, 1, 10), end=datetime(1, 1, 1, 11)),
+            TimeSpan(start=datetime(1, 1, 1, 11), end=datetime(1, 1, 1, 12)),
+            None,
+    ),
+    (
+            TimeSpan(start=datetime(1, 1, 1, 10), end=datetime(1, 1, 1, 12)),
+            TimeSpan(start=datetime(1, 1, 1, 11), end=datetime(1, 1, 1, 13)),
+            TimeSpan(start=datetime(1, 1, 1, 11), end=datetime(1, 1, 1, 12)),
+    ),
+    (
+            TimeSpan(start=datetime(1, 1, 1, 10), end=datetime(1, 1, 1, 13)),
+            TimeSpan(start=datetime(1, 1, 1, 11), end=datetime(1, 1, 1, 12)),
+            TimeSpan(start=datetime(1, 1, 1, 11), end=datetime(1, 1, 1, 12)),
+    ),
+])
+def test_intersection(span1: TimeSpan, span2: TimeSpan, intersection: TimeSpan):
+    actual_intersection1 = span1 & span2
+    actual_intersection2 = span2 & span1
+    assert actual_intersection1 == actual_intersection2
+    assert actual_intersection1 == intersection
+
+
 @pytest.mark.parametrize(
     "span1, span2, difference",
     [
         (
-            TimeSpan(start=datetime(1, 1, 1, 10), end=datetime(1, 1, 1, 11)),
-            TimeSpan(start=datetime(1, 1, 1, 10), end=datetime(1, 1, 1, 11)),
-            (),
+                TimeSpan(start=datetime(1, 1, 1, 10), end=datetime(1, 1, 1, 11)),
+                TimeSpan(start=datetime(1, 1, 1, 10), end=datetime(1, 1, 1, 11)),
+                (),
         ),
         (
-            TimeSpan(start=datetime(1, 1, 1, 10), end=datetime(1, 1, 1, 11)),
-            TimeSpan(start=datetime(1, 1, 1, 9), end=datetime(1, 1, 1, 11)),
-            (),
+                TimeSpan(start=datetime(1, 1, 1, 10), end=datetime(1, 1, 1, 11)),
+                TimeSpan(start=datetime(1, 1, 1, 9), end=datetime(1, 1, 1, 11)),
+                (),
         ),
         (
-            TimeSpan(start=datetime(1, 1, 1, 10), end=datetime(1, 1, 1, 11)),
-            TimeSpan(start=datetime(1, 1, 1, 10), end=datetime(1, 1, 1, 12)),
-            (),
+                TimeSpan(start=datetime(1, 1, 1, 10), end=datetime(1, 1, 1, 11)),
+                TimeSpan(start=datetime(1, 1, 1, 10), end=datetime(1, 1, 1, 12)),
+                (),
         ),
         (
-            TimeSpan(start=datetime(1, 1, 1, 10), end=datetime(1, 1, 1, 12)),
-            TimeSpan(start=datetime(1, 1, 1, 11), end=datetime(1, 1, 1, 13)),
-            (TimeSpan(start=datetime(1, 1, 1, 10), end=datetime(1, 1, 1, 11)),),
+                TimeSpan(start=datetime(1, 1, 1, 10), end=datetime(1, 1, 1, 12)),
+                TimeSpan(start=datetime(1, 1, 1, 11), end=datetime(1, 1, 1, 13)),
+                (TimeSpan(start=datetime(1, 1, 1, 10), end=datetime(1, 1, 1, 11)),),
         ),
         (
-            TimeSpan(start=datetime(1, 1, 1, 10), end=datetime(1, 1, 1, 12)),
-            TimeSpan(start=datetime(1, 1, 1, 11), end=datetime(1, 1, 1, 12)),
-            (TimeSpan(start=datetime(1, 1, 1, 10), end=datetime(1, 1, 1, 11)),),
+                TimeSpan(start=datetime(1, 1, 1, 10), end=datetime(1, 1, 1, 12)),
+                TimeSpan(start=datetime(1, 1, 1, 11), end=datetime(1, 1, 1, 12)),
+                (TimeSpan(start=datetime(1, 1, 1, 10), end=datetime(1, 1, 1, 11)),),
         ),
         (
-            TimeSpan(start=datetime(1, 1, 1, 10), end=datetime(1, 1, 1, 12)),
-            TimeSpan(start=datetime(1, 1, 1, 9), end=datetime(1, 1, 1, 11)),
-            (TimeSpan(start=datetime(1, 1, 1, 11), end=datetime(1, 1, 1, 12)),),
+                TimeSpan(start=datetime(1, 1, 1, 10), end=datetime(1, 1, 1, 12)),
+                TimeSpan(start=datetime(1, 1, 1, 9), end=datetime(1, 1, 1, 11)),
+                (TimeSpan(start=datetime(1, 1, 1, 11), end=datetime(1, 1, 1, 12)),),
         ),
         (
-            TimeSpan(start=datetime(1, 1, 1, 10), end=datetime(1, 1, 1, 12)),
-            TimeSpan(start=datetime(1, 1, 1, 10), end=datetime(1, 1, 1, 11)),
-            (TimeSpan(start=datetime(1, 1, 1, 11), end=datetime(1, 1, 1, 12)),),
+                TimeSpan(start=datetime(1, 1, 1, 10), end=datetime(1, 1, 1, 12)),
+                TimeSpan(start=datetime(1, 1, 1, 10), end=datetime(1, 1, 1, 11)),
+                (TimeSpan(start=datetime(1, 1, 1, 11), end=datetime(1, 1, 1, 12)),),
         ),
         (
-            TimeSpan(start=datetime(1, 1, 1, 10), end=datetime(1, 1, 1, 12)),
-            TimeSpan(start=datetime(1, 1, 1, 10, 30), end=datetime(1, 1, 1, 11, 30)),
-            (
-                TimeSpan(start=datetime(1, 1, 1, 10), end=datetime(1, 1, 1, 10, 30)),
-                TimeSpan(start=datetime(1, 1, 1, 11, 30), end=datetime(1, 1, 1, 12)),
-            ),
+                TimeSpan(start=datetime(1, 1, 1, 10), end=datetime(1, 1, 1, 12)),
+                TimeSpan(start=datetime(1, 1, 1, 10, 30), end=datetime(1, 1, 1, 11, 30)),
+                (
+                        TimeSpan(start=datetime(1, 1, 1, 10), end=datetime(1, 1, 1, 10, 30)),
+                        TimeSpan(start=datetime(1, 1, 1, 11, 30), end=datetime(1, 1, 1, 12)),
+                ),
         ),
     ],
 )
-def test_subtract(span1, span2, difference):
+def test_subtract(span1: TimeSpan, span2: TimeSpan, difference: TimeSpan):
     actual_difference = span1 - span2
     assert actual_difference == difference

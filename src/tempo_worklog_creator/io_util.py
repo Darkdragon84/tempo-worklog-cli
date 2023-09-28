2f90986848ae1bff3fafe61264b455c8c65fcec3
Valentin <v.stauber@parityqc.com>
2023-09-28 15:50:34 +0200

    pull out load/save_yaml

--- src/tempo_worklog_creator/io_util.py
+++ src/tempo_worklog_creator/io_util.py
@@ -10,6 +10,18 @@ yaml.default_flow_style = False  # disable flow style for consistent YAML format
 converter = Converter(forbid_extra_keys=True)
 
 
+def load_yaml(filepath: Path | str) -> dict | list:
+    filepath = Path(filepath)
+    with filepath.open("r") as file:
+        return yaml.load(file)
+
+
+def save_yaml(obj, filepath: Path | str):
+    filepath = Path(filepath)
+    with filepath.open("w") as file:
+        yaml.dump(obj, file)
+
+
 class SaveLoad:
     def to_dict(self) -> dict[str, Any]:
         return converter.unstructure(self)
@@ -19,12 +31,8 @@ class SaveLoad:
         return converter.structure(dct, cls)
 
     @classmethod
-    def from_yaml(cls, filepath: Union[Path, str]):
-        filepath = Path(filepath)
-        with filepath.open("r") as file:
-            return cls.from_dict(yaml.load(file))
+    def from_yaml(cls, filepath: Path | str):
+        return cls.from_dict(load_yaml(filepath))
 
-    def to_yaml(self, filepath: Union[Path, str]):
-        filepath = Path(filepath)
-        with filepath.open("w") as file:
-            yaml.dump(self.to_dict(), file)
+    def to_yaml(self, filepath: Path | str):
+        save_yaml(self.to_dict(), filepath)
